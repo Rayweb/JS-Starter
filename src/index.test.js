ce16231c71c37438982f9398b80f64409b9a30b7
Raymundo Armendariz <raymundo.armendariz@gmail.com>
2018-02-22 15:33:18 -0700

    removing jsdom for now

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,4 @@
 import {expect} from 'chai';
-import jsdom from 'jsdom';
 import fs from 'fs';
 
 describe('Our first tests', () => {
@@ -8,16 +7,6 @@ describe('Our first tests', () => {
     });
 });
 
-describe('Index.html', () => {
-    it('should say hello', (done) => {
-        const index = fs.readFileSync('./src/index.html',"utf-8");
-        jsdom.env(index, function(err,window) {
-            const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Hello world!");
-            done();
-            window.close();
-        });
-    });
-});
+
 
 
